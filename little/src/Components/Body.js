2596041b67eae3751c20a0b5294958745481eab2
Abby Pawelski <abbypawelski@Abbys-MacBook-Air.local>
2024-07-10 16:12:54 -0500

    updated body

--- little/src/Components/Body.js
+++ little/src/Components/Body.js
@@ -11,7 +11,39 @@ const Body = () => {
     <textarea id="Highlights">
         <h2>Specials</h2>
         <button>Online Menu</button>
-            <grid></grid>
+        <textarea>
+        <img src="greek-salad"></img>
+          <h3>Greek Salad</h3>
+          <p>$12.99</p>
+          <p>The famous Greek salad with crispy olives, peppers, cheese and fresh le...</p>
+          <p>Order for Delivery!</p>
+          <img>Bike image</img>
+        </textarea>
+        <textarea>
+        <img src="bruchetta"></img>
+          <h3>Bruchetta</h3>
+          <p>$7.99</p>
+          <p>Our Bruchetta is made with grilled bread and brushed with garlic and...</p>
+          <p>Order for Delivery!</p>
+          <img>Bike image</img>
+        </textarea>
+        <textarea>
+            <img src="vegetarian-ziti"></img>
+          <h3>Vegetarian Ziti</h3>
+          <p>$11.99</p>
+          <p>Freshly made ziti with a rich tomatoes and basil sauce topped wi...</p>
+          <p>Order for Delivery!</p>
+          <img>Bike image</img>
+        </textarea>
+    </textarea>
+    <textarea id="testimonials">
+        <h1>Testimonials</h1>
+    </textarea>
+    <textarea id="about">
+
+    </textarea>
+    <textarea id="footer">
+
     </textarea>
 </body>
     );
