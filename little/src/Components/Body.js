9e31e30b20cbb9e9908a7b114e904b5d4b2b15cd
Abby Pawelski <abbypawelski@Abbys-MacBook-Air.local>
2024-07-10 16:04:50 -0500

    updated body and app

--- little/src/Components/Body.js
+++ little/src/Components/Body.js
@@ -1,5 +1,18 @@
 const Body = () => {
     return(
-<body></body>
+<body>
+    <textarea id="Hero-section">
+        <h1>Little Lemon</h1>
+        <h2>Chicago</h2>
+        <p>We are a family owned Mediteranian resturant, focused on traditional recipes served with a modern twist.</p>
+        <button>Reserve a table</button>
+        <img src="chef-holding-tapas.jpg"></img>
+    </textarea>
+    <textarea id="Highlights">
+        <h2>Specials</h2>
+        <button>Online Menu</button>
+            <grid></grid>
+    </textarea>
+</body>
     );
 }
