061463628bc7f2fec977ed4abfeac9f6ac841dff
Abby Pawelski <abbypawelski@Abbys-MacBook-Air.local>
2024-07-14 12:33:36 -0500

    basic html skeleton completed, now ready to convert to react components

--- little/src/Components/Body.js
+++ little/src/Components/Body.js
@@ -1,29 +1,29 @@
 const Body = () => {
     return(
 <body>
-    <textarea id="Hero-section">
+    <section id="Hero-section">
         <h1>Little Lemon</h1>
         <h2>Chicago</h2>
         <p>We are a family owned Mediteranian resturant, focused on traditional recipes served with a modern twist.</p>
         <button>Reserve a table</button>
         <img src="chef-holding-tapas.jpg"></img>
-    </textarea>
-    <textarea id="Highlights">
+    </section>
+    <section id="Highlights">
         <h2>Specials</h2>
         <button>Online Menu</button>
         <textarea>
         <img src="greek-salad"></img>
           <h3>Greek Salad</h3>
           <p>$12.99</p>
-          <p>The famous Greek salad with crispy olives, peppers, cheese and fresh le...</p>
+          <p>The famous Greek salad with crispy olives, peppers, cheese and fresh lemons.</p>
           <p>Order for Delivery!</p>
           <img>Bike image</img>
         </textarea>
         <textarea>
         <img src="bruchetta"></img>
-          <h3>Bruchetta</h3>
+          <h3>Bruschetta</h3>
           <p>$7.99</p>
-          <p>Our Bruchetta is made with grilled bread and brushed with garlic and...</p>
+          <p>Our Bruchetta is made with grilled bread and brushed with olive oil, sea salt, and topped with fresh heirloom tomatoes.</p>
           <p>Order for Delivery!</p>
           <img>Bike image</img>
         </textarea>
@@ -31,20 +31,52 @@ const Body = () => {
             <img src="vegetarian-ziti"></img>
           <h3>Vegetarian Ziti</h3>
           <p>$11.99</p>
-          <p>Freshly made ziti with a rich tomatoes and basil sauce topped wi...</p>
+          <p>Freshly made ziti with a rich tomatoes and basil sauce topped with basil and our secret mediterranean spice blend.</p>
           <p>Order for Delivery!</p>
           <img>Bike image</img>
         </textarea>
-    </textarea>
-    <textarea id="testimonials">
+    </section>
+    <section id="testimonials">
         <h1>Testimonials</h1>
-    </textarea>
-    <textarea id="about">
+        <textarea id="review-marcus">
+            <h2>Rating</h2>
+            <img>Lemon Rating image 4</img>
+            <img>Marcus</img>
+            <h2>Marcus</h2>
+            <p>“Great date spot! My wife and I really enjoyed the atmosphere.”</p>
+        </textarea>
+        <textarea id="review-mosalowa">
+            <h2>Rating</h2>
+            <img>Lemon Rating image 3.5</img>
+            <img>Mosalowa</img>
+            <h2>Mosalowa</h2>
+            <p>“Had a good time here with my family. Service was fast.”</p>
+        </textarea>
+        <textarea id="review-ann">
+            <h2>Rating</h2>
+            <img>Lemon Rating image 5</img>
+            <img>Ann</img>
+            <h2>Ann</h2>
+            <p>“Best Mediterranean food in Chicago!”</p>
+        </textarea>
+        <textarea id="review-dimitri">
+            <h2>Rating</h2>
+            <img>Lemon Rating image 4</img>
+            <img>Dimitri</img>
+            <h2>Dimitri</h2>
+            <p>“My coworkers and I like grabbing a quick lunch here.”</p>
+        </textarea>
+    </section>
+    <section id="about">
+        <h1>Little Lemon</h1>
+        <h2>Chicago</h2>
+        <p>Little Lemon opened in 1995 by two Italian brothers, Adrian and Mario. Despite the city's diversity, the two brothers recognized the lack of Mediterranean cuisine in Chicago,  and were inspired to bring the flavors of their hometown in Italy to the people of Chicago. The two brothers continue to oversee the Little Lemon restaurant, nearly thirty years later.</p>
+        <img>Adrian and Mario A</img>
+        <img>Adrian and Mario B</img>
+    </section>
+    <section id="footer">
 
-    </textarea>
-    <textarea id="footer">
-
-    </textarea>
+    </section>
 </body>
     );
 }
