1661c1f736dff85633d12c370b2196ae07763440
Abby Pawelski <abbypawelski@Abbys-MacBook-Air.local>
2024-07-17 11:41:53 -0500

    adding routing for navigation

--- little/src/index.js
+++ little/src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./Pages/About";
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import Menu from "./Pages/Menu";
+import OrderOnline from "./Pages/OrderOnline";
+import Reservations from "./Pages/Reservations";
+import NoPage from "./Pages/NoPage";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home></Home>}></Route>
+        <Route path = "/" element={<About></About>}></Route>
+        <Route path = "login" element={<Login></Login>}></Route>
+        <Route path = "menu" element={<Menu></Menu>}></Route>
+        <Route path = "orderOnline" element={<OrderOnline></OrderOnline>}></Route>
+        <Route path = "reservations" element={<Reservations></Reservations>}></Route>
+        <Route path ="*" element={<NoPage></NoPage>}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<App />);
