072bc28ebff94e158cea83c327995343466a92ba
Abby Pawelski <abbypawelski@Abbys-MacBook-Air.local>
2024-08-28 20:56:50 -0500

    fixed spelling errors

--- little/src/Components/AboutHome.js
+++ little/src/Components/AboutHome.js
@@ -11,7 +11,7 @@ const AboutHome = () => {
             <p>Little Lemon opened in 1995 by two Italian brothers, Adrian and Mario. Despite the city's diversity, the two brothers recognized the lack of Mediterranean cuisine in Chicago,  and were inspired to bring the flavors of their hometown in Italy to the people of Chicago. The two brothers continue to oversee the Little Lemon restaurant, nearly thirty years later.</p>
         </div>
         <div>
-            <img alt="Two brothers, Adrian and Mario stand behind a woodden kitchen counter" src={AdrianAndMario}></img>
+            <img alt="Two brothers, Adrian and Mario stand behind a wooden kitchen counter" src={AdrianAndMario}></img>
         </div>
 </section>
     )
